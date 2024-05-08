d6ca3f5445cc27043bc56c171b92c115df7a6313
Honorato <dev.honorato.web@gmail.com>
2024-05-07 23:57:00 -0300

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EstruturaComponent } from './layout/components/estrutura/estrutura.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { VazioComponent } from './components/vazio/vazio.component';
 
 const routes: Routes = [
   {path: '', component: EstruturaComponent,
-  // children: [
-  //   { path: '', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-  //   { path: 'set/categorias', component: CategoriasComponent, loadChildren: () => import('./components/set/categorias/categorias.module').then(m => m.CategoriasModule) },
-  }
+  children: [
+    { path: '', component: DashboardComponent, loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) },
+    { path: 'set/vazio', component: VazioComponent, loadChildren: () => import('./components/vazio/vazio.module').then(m => m.VazioModule) },
+  ]}
 ];
 
 @NgModule({
